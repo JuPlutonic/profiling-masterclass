8bd67226cf8add9c1dbc80ce27705ad7dc48c3cb
azhevlakov <azhevlakov@ceki.ru>
2021-01-22 03:04:52 +0300

    FIXMEs ⏲[9], data.txt(results.json)-changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+# FIXME: send parameters to get route
   get '/report', action: :report, controller: 'report', defaults: { start_date: '2015-01-01', finish_date: '2021-01-02' }
 end
