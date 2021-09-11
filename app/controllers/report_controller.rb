fa1ea8225040065362d13b3b812a7625e097d30e
azhevlakov <azhevlakov@ceki.ru>
2021-09-11 13:05:02 +0300

    Fixed comments, stat_browsers_part. Prof methods extracted

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,7 +2,6 @@
 
 # Controller for report
 class ReportController < ApplicationController
-  LOGGER_BAR = ('*' * 40).freeze
   DEF_PARAMS_START_DATE = '2015-07-01'.freeze
   DEF_PARAMS_END_DATE = '2021-12-12'.freeze
 
@@ -15,11 +14,6 @@ class ReportController < ApplicationController
     @start_date = Date.parse(params.fetch(:start_date, DEF_PARAMS_START_DATE))
     @finish_date = Date.parse(params.fetch(:finish_date, DEF_PARAMS_END_DATE))
 
-
-# Todo's TOPIC: Refactor sessions' loading in more "stream/thread"-style
-# TODO: In certain moment all 90MB (with AR wrappings it's more) of sessions DB table can be loaded to memory. So let's implement records loading in batches
-#   An algorithm will extract all needed data from a batch, data will be kept batch will be replaced with next batch
-=end
     sessions_by_dates = Session.where(
       'date >= :start_date AND date <= :finish_date',
       start_date: @start_date,
@@ -28,84 +22,55 @@ class ReportController < ApplicationController
 
     # @unique_browsers_count = unique_browsers_count(sessions_by_dates)
 
-    sl = sessions_by_dates.pluck(:user_id).uniq
+    load_sessions_ids = sessions_by_dates.pluck(:user_id).uniq
+
     users =
       User
-      .where(id: sl)
+      .where(id: load_sessions_ids)
         .order(:id)
         .limit(30)
 
-    sessions = sessions_by_dates.where(user_id: sl)
-    @unique_browsers_count = unique_browsers_count(sessions)
+    sessions = sessions_by_dates.where(user_id: load_sessions_ids)
 
+    @unique_browsers_count = unique_browsers_count(sessions)
     @total_users = users.count
     @total_sessions = sessions.count
-
-    # TODO: now it is safe to uncomment it (but we need to seed more users/sessions, gem valid_email2 alr. configured):
-    # users_array = select_valid_users(users)
-    @users = []
-
-    load_users(users).each do |user|
-      user_sessions = select_sessions_of_user(user, load_sessions(sessions))
-      @users += [stats_for_user(user, user_sessions)]
-    end
+    work(users, sessions)
   end
 
   private
-
-  # We will use visualization in a browser in flame-graph format https://speedscope.app
-  def profile_with_stackprof(&block)
-    # return block.call if params[:profile] != 'json'
-    profile = StackProf.run(mode: :wall, raw: true, &block)
-    File.write('tmp/stackprof.json', JSON.generate(profile))
-  end
-
-  # Legent: rss - Resident Set Size
-  # Amount of RAM in MB, assigned to process
-  def mem_usage
-    `ps -o rss= -p #{Process.pid}`.to_i / 1_024
-  end
-
-  def log_memory_usage(mem)
-    Rails.logger.info(LOGGER_BAR + "\n" + "#{mem} MB".rjust(96) + "\n" + LOGGER_BAR.rjust(129))
-  end
-
-  def wrap_in_mem_prof
-    GC.start(full_mark: true, immediate_sweep: true)
-    GC.disable
-    mem_before = mem_usage
-
-    yield
-
-    mem_after = mem_usage
-    log_memory_usage(mem_after - mem_before)
-  end
-
-  def profile_with_ruby_prof
-    RubyProf.measure_mode = RubyProf::MEMORY
-    profile = RubyProf.profile { yield }
-    printer = RubyProf::CallTreePrinter.new(profile)
-    printer.print(path: 'tmp', profile: 'rubyprof')
-  end
 
   def accept_all_params
     params.permit!
   end
 
   def unique_browsers_count(sessions)
-    sessions.map(&:browser).map(&:upcase).uniq.count
+    sessions.map(&:browser).uniq.count
     # sessions.select(&:browser).distinct.count
   end
 
+  def work(users, sessions)
+    @users = []
+
+    load_valid_users(users).each do |user|
+      user_sessions = select_sessions_of_user(user, load_sessions(sessions))
+      @users += [stats_for_user(user, user_sessions)]
+    end
+  end
+
   ##
-  ## auxiliary variables for @users
+  ## auxiliary variables for #work
   ##
   def select_valid_users(users)
     users.select(&:valid?)
   end
 
   def load_users(users)
-    # users.to_a
+    users.to_a
+  end
+
+  # TODO: we need to seed more users/sessions, gem valid_email2 alr. configured
+  def load_valid_users(users)
     select_valid_users(users)
   end
 
@@ -113,12 +78,9 @@ class ReportController < ApplicationController
     sessions.to_a
   end
 
-  # def load_batch_of_sessions(sessions, batch_number:, size: 1_000)
-  #   sessions.limit(size).offset(batch_number * size)
-  # end
-  ##
-  ##
-  ##
+  def load_batch_of_sessions(sessions, batch_number:, size: 1_000)
+    sessions.limit(size).offset(batch_number * size)
+  end
 
   def select_sessions_of_user(user, sessions_array)
     # sessions_array.select { |session| session.user_id == user.id }
@@ -129,27 +91,27 @@ class ReportController < ApplicationController
   # :reek:FeatureEnvy
   def stats_for_user(user, user_sessions)
     duration_of_sessions = user_sessions.map(&:duration)
+    browsers_list = user_sessions.map(&:browser).map(&:upcase)
     { first_name: user.first_name,
       last_name: user.last_name,
       sessions_count: user_sessions.count,
       total_time: "#{duration_of_sessions.sum} min.",
       longest_session: "#{duration_of_sessions.max} min.",
-      browsers: stats_browsers_part(user_sessions),
-      used_ie: stats_browsers_part(user_sessions, used: 'ie'),
-      used_only_chrome: stats_browsers_part(user_sessions, used: 'chrome'),
+      browsers: stats_browsers_part(browsers_list),
+      used_ie: stats_browsers_part(browsers_list, used: 'ie'),
+      used_only_chrome: stats_browsers_part(browsers_list, used: 'chrome'),
       dates: user_sessions.map(&:date).map(&:to_s).sort.uniq
     }
   end
 
-  def stats_browsers_part(user_sessions, used: 'all')
-    get_browsers_list = user_sessions.map(&:browser).map(&:upcase)
+  def stats_browsers_part(browsers_list, used: 'all')
     case used
     when 'chrome'
-      get_browsers_list.all? { |browser| browser =~ /CHOME/ }
+      browsers_list.all? { |browser| browser =~ /CHOME/ }
     when 'ie'
-      get_browsers_list.any? { |browser| browser =~ /INTERNET_EXPLORER/ }
+      browsers_list.any? { |browser| browser =~ /INTERNET EXPLORER/ }
     else
-      get_browsers_list.sort.uniq
+      browsers_list.sort.uniq
     end
   end
 end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,56 @@
+module ApplicationHelper
+  LOGGER_BAR = ('*' * 40).freeze
+
+  # We will use visualization in a browser in flame-graph format https://speedscope.app
+  def self.profile_with_stackprof(&block)
+    profile = StackProf.run(mode: :wall, raw: true, &block)
+    File.write('tmp/stackprof.json', JSON.generate(profile))
+  end
+
+  def self.wrap_in_mem_prof
+    mem_before = mem_usage
+    yield
+
+    mem_after = mem_usage
+    log_memory_usage(mem_after - mem_before)
+  end
+
+  def self.profile_with_ruby_prof
+    RubyProf.measure_mode = RubyProf::MEMORY
+    profile = RubyProf.profile { yield }
+    printer = RubyProf::CallTreePrinter.new(profile)
+    printer.print(path: 'tmp', profile: 'rubyprof')
+  end
+
+  def profile_with_stackprof(&block)
+    ApplicationHelper.profile_with_stackprof(&block)
+  end
+
+  def wrap_in_mem_prof(&block)
+    GC.start(full_mark: true, immediate_sweep: true)
+    GC.disable
+    ApplicationHelper.wrap_in_mem_prof(&block)
+  end
+
+  def profile_with_ruby_prof(&block)
+    ApplicationHelper.profile_with_ruby_prof(&block)
+  end
+
+  private
+
+  # Legent: rss - Resident Set Size
+  # Amount of RAM in MB, assigned to process
+  def self.mem_usage
+    `ps -o rss= -p #{Process.pid}`.to_i / 1_024
+  end
+
+  def self.log_memory_usage(mem)
+    text = case Rails.env
+           when 'development', 'test'
+             "#{mem} MB" + "\n" + LOGGER_BAR
+           else
+             "#{mem} MB".rjust(96) + "\n" + LOGGER_BAR.rjust(129)
+    end
+    Rails.logger.info(LOGGER_BAR + "\n" + text)
+  end
+end
